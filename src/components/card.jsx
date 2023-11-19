702284e0aaf319a6347235085c68be7e406ef2d7
gmw43 <148313421+gmw43@users.noreply.github.com>
2023-11-19 17:54:09 +0500

    commiit

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,12 +4,12 @@ import CardItems from './card-items'
 const Card = () => {
     return (
         <>
-            {/* <div className="flex flex-wrap justify-center items-center h-screen bg-gray-200"> */}
+            <div className="flex flex-wrap justify-center items-center h-screen bg-gray-200">
                 <CardItems imageUrl="https://placekitten.com/300/200" // Replace with your actual image URL
                     name="Sample Name"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                      />
-            {/* </div> */}
+            </div>
         </>
 
     )
