78438a6dd066561855eef435515ad1a19e748211
gmw43 <148313421+gmw43@users.noreply.github.com>
2023-11-19 17:42:01 +0500

    commit change

--- src/components/card-items.jsx
+++ src/components/card-items.jsx
@@ -3,8 +3,11 @@
 import React from 'react';
 
 const CardItems = ({ imageUrl, name, description,  }) => {
+
+  
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden bg-gray-200 shadow-lg ">
       <img className="w-full" src={imageUrl} alt={name} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
@@ -12,7 +15,7 @@ const CardItems = ({ imageUrl, name, description,  }) => {
       </div>
       <div className="px-6 py-4">
         <button
-         
+         onClick={() => alert(`Added ${name} to Cart`)} 
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           Add
